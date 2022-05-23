b46c527eb7897b4a64e2d9e2f8169dcacb9d64b0
Hannah Poeng <hannah.poeng@yale.edu>
2022-05-23 02:23:11 -0400

    commit tree 60d5aa77baab42cf20dfd6594983749f1083b73e

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,23 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end
+
+say_hello_world_ten_times
+
+def greeting # Method Signature
+    puts "Hello World" # Method Body
+end # Method Closing
+
+greeting # Executing the method by name
